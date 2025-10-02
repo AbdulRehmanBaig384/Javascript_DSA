1a7a72f205ba19f3bf85114dbb040a35529e844c
ABDUL REHMAN BAIG <rehmanbaig456@gmail.com>
2025-10-02 22:37:58 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,147 @@
+// let a=3
+// let b=4
+// let c =a+b
+// console.log(c)
+
+// let arr=[1,2,3,4,14,13]
+//     let max=arr[0]
+//     for(let i=0;i<=arr.length;i++){
+//         if(arr[i]>max){
+//             max=arr[i]
+            
+//         }
+       
+//     }
+//     console.log(max)
+
+
+
+// let a=2
+// if(a%2==0){
+//     console.log('number is even')
+// }
+// else{
+//     console.log('number is odd')
+// }
+
+// for(let i=0;i<=10;i++){
+//     console.log(i)
+// }
+
+// let n=5;
+// for (let i=1;i<=10;i++){
+//     let sum=n*i
+
+//     console.log(sum)
+// }
+
+// let array=[1,-2,3,4,-5]
+// let min =array[0]
+// for (let i=0;i<array.length;i++){
+//   if(array[i]<min){
+//     min=array[i]
+    
+//   }
+// }
+// console.log(min)
+
+// let number=5
+// let numbers
+// for(let i=4;i>=1;i--){
+//    number=number*i
+
+// }
+// console.log(number)
+
+
+// let sum=0
+// for(let i=0;i<=100;i++){
+//    sum+=i
+// }
+// console.log(sum)
+
+
+// let words='hello world'
+// for(let i=words.length;i>=0;i--){
+//    console.log(words[i])
+// }
+
+// let a=1
+// let b=0
+// n=10
+// console.log(b)
+// console.log(a)
+// for(let i=2;i<n;i++){
+//    let next=a+b
+//    console.log(next)
+//    b=a;
+//    a=next
+// };
+
+
+
+
+// let a=30
+// let b=40
+// a=a+b
+// b=a-b
+// a=a-b
+
+// console.log(a,b)
+
+// let c=true
+// c++
+// console.log(c)
+// console.log(Math.round((Math.random()*5000+1000)))
+
+// // var ans=Number(prompt("Enter your age"))
+// // console.log(ans)
+
+// 12>3?console.log(true):console.log(false)
+
+
+// for(let i=1;i<=10;i++){
+//         console.log('Multiplication table of',i)
+
+//     for(let j=1;j<=10;j++){
+     
+//         console.log(`${i} * ${j} =`,i*j)
+//     }
+// }
+
+
+// for(let i=0;i<=4;i++){
+//     let row=''
+//     for(let j=1;j<=3;j++){
+//         row+='*'
+//       }
+//       console.log(row)
+// }
+
+
+// for(let i=1;i<=4;i++){
+//     let row=''
+//     for(let j=i;j>=1;j--){
+     
+//         row+='*'
+//     }
+//     console.log(row)
+// }
+
+
+// for(let i=1;i<=4;i++){
+//     let row=''
+//     for(let j=4;j>=i;j--){
+//         row+='*'
+//     }
+//     console.log(row)
+// }
+
+
+for(let i=1;i<=4;i++){
+    let row=''
+    for(let j=1;j<=i;j++){
+        row+=j
+    }
+    console.log(row)
+}
